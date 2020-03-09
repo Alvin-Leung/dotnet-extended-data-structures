1ff0cbd1d92e6c2cee912c2b76f60a215a29c1eb
alvin <alvin.leung92@gmail.com>
2020-03-08 23:01:40 -0700

    Implement basic union find methods. Add unit tests for Find and Union methods.

--- DataStructures/UnionFind.cs
+++ DataStructures/UnionFind.cs
@@ -1,29 +1,70 @@
 ﻿using System;
+using System.Linq;
 
 namespace DataStructures
 {
     public class UnionFind
     {
-        public int Count { get; set; }
+        private int[] elements;
+        private int numComponents;
+
+        public int Count => this.numComponents;
 
         public UnionFind(int size)
         {
-            throw new NotImplementedException();
+            this.elements = Enumerable.Range(0, size).ToArray();
+            this.numComponents = size;
         }
 
-        public void Connected(int firstIndex, int secondIndex)
+        public bool Connected(int firstIndex, int secondIndex)
         {
-            throw new NotImplementedException();
+            return this.Find(firstIndex) == this.Find(secondIndex);
         }
 
         public int Find(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > this.Count - 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), Resource.IndexMustBeWithinValidRange);
+            }
+
+            int nextIndex = index;
+
+            while (this.elements[nextIndex] != nextIndex)
+            {
+                nextIndex = this.elements[index];
+            }
+
+            return nextIndex;
         }
 
         public void Union(int firstIndex, int secondIndex)
         {
-            throw new NotImplementedException();
+            if (firstIndex < 0 || firstIndex > this.Count - 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(firstIndex), Resource.IndexMustBeWithinValidRange);
+            }
+
+            if (secondIndex < 0 || secondIndex > this.Count - 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(secondIndex), Resource.IndexMustBeWithinValidRange);
+            }
+
+            if (firstIndex == secondIndex)
+            {
+                return;
+            }
+
+            var firstParentIndex = this.Find(firstIndex);
+            var secondParentIndex = this.Find(secondIndex);
+
+            if (firstParentIndex == secondParentIndex)
+            {
+                return;
+            }
+
+            this.elements[firstParentIndex] = secondParentIndex;
+            this.numComponents--;
         }
     }
 }
--- UnitTests/UnionFindTests.cs
+++ UnitTests/UnionFindTests.cs
@@ -1,16 +1,40 @@
 using DataStructures;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace UnitTests
 {
     public class UnionFindTests
     {
-        [TestCase(1, 1, 1)]
+        [TestCase(1, 0, 0)]
+        [TestCase(2, 1, 1)]
+        [TestCase(4, 3, 3)]
+        [TestCase(10, 9, 9)]
         [Test]
         public void TestFind(int size, int indexToFind, int expectedParent)
         {
             var unionFind = new UnionFind(size);
             Assert.That(unionFind.Find(indexToFind), Is.EqualTo(expectedParent));
+        }
+
+        [TestCaseSource(nameof(UnionFindTests.UnionTestCaseSource))]
+        public void TestUnion(Tuple<int, int>[] pairsToMerge, int indexToFind, int expectedParent)
+        {
+            var unionFind = new UnionFind(1000);
+
+            foreach (var pair in pairsToMerge)
+            {
+                unionFind.Union(pair.Item1, pair.Item2);
+            }
+
+            Assert.That(unionFind.Find(indexToFind), Is.EqualTo(expectedParent));
+        }
+
+        private static IEnumerable<TestCaseData> UnionTestCaseSource()
+        {
+            yield return new TestCaseData(new[] { new Tuple<int, int>(1, 1) }, 1, 1);
+            yield return new TestCaseData(new[] { new Tuple<int, int>(1, 2) }, 1, 2);
         }
     }
 }
