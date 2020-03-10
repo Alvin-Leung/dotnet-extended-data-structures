05cd529af4703afeeacf77c911c844721227e8f9
alvin <alvin.leung92@gmail.com>
2020-03-09 22:50:06 -0700

    Inject UnionFind size through test parameters

--- UnitTests/UnionFindTests.cs
+++ UnitTests/UnionFindTests.cs
@@ -20,7 +20,7 @@ namespace UnitTests
         [TestCaseSource(nameof(UnionFindTests.UnifyTestCaseSource))]
         public void TestUnify(TestParameters parameters)
         {
-            var unionFind = new UnionFind(1000);
+            var unionFind = new UnionFind(parameters.Size);
 
             foreach (var pair in parameters.PairsToMerge)
             {
@@ -37,24 +37,28 @@ namespace UnitTests
         {
             yield return new TestCaseData(new TestParameters
             {
+                Size = 4,
                 PairsToMerge = new[] { new IndexPair(1, 1), new IndexPair(2, 2), new IndexPair(3, 3) },
                 TestPairs = new[] { new TestPair(1, 1), new TestPair(2, 2), new TestPair(3, 3) }
             }).SetName("Merge elements to themselves");
 
             yield return new TestCaseData(new TestParameters
             {
+                Size = 6,
                 PairsToMerge = new[] { new IndexPair(1, 2), new IndexPair(2, 3), new IndexPair(3, 4), new IndexPair(4, 5) },
                 TestPairs = new[] { new TestPair(1, 1), new TestPair(2, 1), new TestPair(3, 1), new TestPair(4, 1), new TestPair(5, 1) }
             }).SetName("Merge all elements to the same group");
 
             yield return new TestCaseData(new TestParameters
             {
+                Size = 101,
                 PairsToMerge = new[] { new IndexPair(5, 100), new IndexPair(3, 50), new IndexPair(2, 50), new IndexPair(50, 6), new IndexPair(100, 6) },
                 TestPairs = new[] { new TestPair(5, 3), new TestPair(100, 3), new TestPair(3, 3), new TestPair(50, 3), new TestPair(2, 3), new TestPair(6, 3) }
             }).SetName("Merge two groups together");
 
             yield return new TestCaseData(new TestParameters
             {
+                Size = 501,
                 PairsToMerge = new[] { new IndexPair(500, 400), new IndexPair(20, 60), new IndexPair(33, 67), new IndexPair(33, 6), new IndexPair(500, 20), new IndexPair(500, 6) },
                 TestPairs = new[] 
                 { 
@@ -70,6 +74,8 @@ namespace UnitTests
 
         public class TestParameters
         {
+            public int Size;
+
             public IndexPair[] PairsToMerge;
 
             public TestPair[] TestPairs;
