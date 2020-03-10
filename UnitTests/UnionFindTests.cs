0981431b04c0d8971278b9a0c15d3101178fd4d9
alvin <alvin.leung92@gmail.com>
2020-03-09 22:42:03 -0700

    Make unit test helper classes more generic

--- UnitTests/UnionFindTests.cs
+++ UnitTests/UnionFindTests.cs
@@ -18,7 +18,7 @@ namespace UnitTests
         }
 
         [TestCaseSource(nameof(UnionFindTests.UnifyTestCaseSource))]
-        public void TestUnify(UnifyTestParameters parameters)
+        public void TestUnify(TestParameters parameters)
         {
             var unionFind = new UnionFind(1000);
 
@@ -27,52 +27,52 @@ namespace UnitTests
                 unionFind.Unify(pair.FirstIndex, pair.SecondIndex);
             }
 
-            foreach (var findResult in parameters.ExpectedFindResult)
+            foreach (var pair in parameters.TestPairs)
             {
-                Assert.That(unionFind.Find(findResult.FindIndex), Is.EqualTo(findResult.RootParentIndex));
+                Assert.That(unionFind.Find(pair.Input), Is.EqualTo(pair.ExpectedOutput));
             }
         }
 
         private static IEnumerable<TestCaseData> UnifyTestCaseSource()
         {
-            yield return new TestCaseData(new UnifyTestParameters
+            yield return new TestCaseData(new TestParameters
             {
                 PairsToMerge = new[] { new IndexPair(1, 1), new IndexPair(2, 2), new IndexPair(3, 3) },
-                ExpectedFindResult = new[] { new FindResult(1, 1), new FindResult(2, 2), new FindResult(3, 3) }
+                TestPairs = new[] { new TestPair(1, 1), new TestPair(2, 2), new TestPair(3, 3) }
             }).SetName("Merge elements to themselves");
 
-            yield return new TestCaseData(new UnifyTestParameters
+            yield return new TestCaseData(new TestParameters
             {
                 PairsToMerge = new[] { new IndexPair(1, 2), new IndexPair(2, 3), new IndexPair(3, 4), new IndexPair(4, 5) },
-                ExpectedFindResult = new[] { new FindResult(1, 1), new FindResult(2, 1), new FindResult(3, 1), new FindResult(4, 1), new FindResult(5, 1) }
+                TestPairs = new[] { new TestPair(1, 1), new TestPair(2, 1), new TestPair(3, 1), new TestPair(4, 1), new TestPair(5, 1) }
             }).SetName("Merge all elements to the same group");
 
-            yield return new TestCaseData(new UnifyTestParameters
+            yield return new TestCaseData(new TestParameters
             {
                 PairsToMerge = new[] { new IndexPair(5, 100), new IndexPair(3, 50), new IndexPair(2, 50), new IndexPair(50, 6), new IndexPair(100, 6) },
-                ExpectedFindResult = new[] { new FindResult(5, 3), new FindResult(100, 3), new FindResult(3, 3), new FindResult(50, 3), new FindResult(2, 3), new FindResult(6, 3) }
+                TestPairs = new[] { new TestPair(5, 3), new TestPair(100, 3), new TestPair(3, 3), new TestPair(50, 3), new TestPair(2, 3), new TestPair(6, 3) }
             }).SetName("Merge two groups together");
 
-            yield return new TestCaseData(new UnifyTestParameters
+            yield return new TestCaseData(new TestParameters
             {
                 PairsToMerge = new[] { new IndexPair(500, 400), new IndexPair(20, 60), new IndexPair(33, 67), new IndexPair(33, 6), new IndexPair(500, 20), new IndexPair(500, 6) },
-                ExpectedFindResult = new[] 
+                TestPairs = new[] 
                 { 
-                    new FindResult(500, 500),
-                    new FindResult(400, 500),
-                    new FindResult(20, 500),
-                    new FindResult(60, 500),
-                    new FindResult(33, 500),
-                    new FindResult(6, 500) 
+                    new TestPair(500, 500),
+                    new TestPair(400, 500),
+                    new TestPair(20, 500),
+                    new TestPair(60, 500),
+                    new TestPair(33, 500),
+                    new TestPair(6, 500) 
                 }
             }).SetName("Merge three groups together");
         }
 
-        public class UnifyTestParameters
+        public class TestParameters
         {
             public IndexPair[] PairsToMerge;
 
-            public FindResult[] ExpectedFindResult;
+            public TestPair[] TestPairs;
         }
 
         public class IndexPair
@@ -88,16 +88,16 @@ namespace UnitTests
             }
         }
 
-        public class FindResult
+        public class TestPair
         {
-            public int FindIndex { get; }
+            public int Input { get; }
 
-            public int RootParentIndex { get; }
+            public int ExpectedOutput { get; }
 
-            public FindResult(int findIndex, int rootParentIndex)
+            public TestPair(int inputIndex, int expectedOutput)
             {
-                this.FindIndex = findIndex;
-                this.RootParentIndex = rootParentIndex;
+                this.Input = inputIndex;
+                this.ExpectedOutput = expectedOutput;
             }
         }
     }
