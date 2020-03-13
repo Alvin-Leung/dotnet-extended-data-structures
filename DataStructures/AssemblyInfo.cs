8daeead83bbfa34d22905099823f3f7d9cba8e4a
alvin <alvin.leung92@gmail.com>
2020-03-12 22:57:38 -0700

    Add AssemblyInfo.cs file. Allow internals of DataStructures project to be visible to UnitTests project

---
+++ DataStructures/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("DataStructures")]
+[assembly: AssemblyDescription("Implementations of data structures that are not included with .NET. Written in C#.")]
+[assembly: AssemblyProduct("DataStructures")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: InternalsVisibleTo("UnitTests")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
