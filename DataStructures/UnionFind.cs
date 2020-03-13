7076d99ff33d91aa2d7753db34227303515e0fd6
alvin <alvin.leung92@gmail.com>
2020-03-12 23:20:01 -0700

    Add xml comments to UnionFind

--- DataStructures/UnionFind.cs
+++ DataStructures/UnionFind.cs
@@ -2,13 +2,28 @@
 
 namespace DataStructures
 {
+    /// <summary>
+    /// An integer based implementation of the union find data structure
+    /// </summary>
+    /// <remarks>
+    /// It is expected that the integer indices stored internally in this <see cref="UnionFind"/> class are correlated to actual objects via a bijection of integer indices 
+    /// to objects
+    /// </remarks>
     public class UnionFind
     {
         private int[] elements;
         private int[] componentSizes;
 
+        /// <summary>
+        /// The number of components that elements have been grouped into
+        /// </summary>
         public int ComponentCount { get; private set; }
 
+        /// <summary>
+        /// Creates an instance of an integer based <see cref="UnionFind"/> data structure
+        /// </summary>
+        /// <param name="size">The number of elements to initialize the <see cref="UnionFind"/> instance with</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if the inputted <paramref name="size"/> is less than 0 or above <see cref="int.MaxValue"/></exception>
         public UnionFind(int size)
         {
             if (size < 0 || size > int.MaxValue)
@@ -28,11 +43,28 @@ namespace DataStructures
             this.ComponentCount = size;
         }
 
+        /// <summary>
+        /// Checks by index whether two elements are in the same group
+        /// </summary>
+        /// <param name="firstIndex">The index of the first element to check</param>
+        /// <param name="secondIndex">The index of the second element to check</param>
+        /// <returns>True if the elements are in the same group, otherwise false</returns>
         public bool Connected(int firstIndex, int secondIndex)
         {
             return this.Find(firstIndex) == this.Find(secondIndex);
         }
 
+        /// <summary>
+        /// Finds the index of the root parent to an element
+        /// </summary>
+        /// <param name="index">The index of the child element</param>
+        /// <returns>The index of the root parent element, or the inputted index if the element has no parents</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if the inputted <paramref name="index"/> is less than 0 or above <see cref="int.MaxValue"/></exception>
+        /// <remarks>
+        /// This method has the side-effect of compressing the path from child to root parent such that all elements between, including the child, directly
+        /// point to the root parent after <see cref="Find(int)"/> has executed. As a result of path compression, <see cref="Find(int)"/> runs in amortized 
+        /// constant time.
+        /// </remarks>
         public int Find(int index)
         {
             if (index < 0 || index > this.elements.Length - 1)
@@ -52,11 +84,24 @@ namespace DataStructures
             return nextIndex;
         }
 
+        /// <summary>
+        /// Gets the size of the component an element belongs to
+        /// </summary>
+        /// <param name="index">The index of the element</param>
+        /// <returns>The size of the component the element belongs to</returns>
         public int GetComponentSize(int index)
         {
             return this.componentSizes[this.Find(index)];
         }
 
+        /// <summary>
+        /// Merges two elements' groups together
+        /// </summary>
+        /// <param name="firstIndex">The index of the first element to merge</param>
+        /// <param name="secondIndex">The index of the second element to merge</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if either the inputted <paramref name="firstIndex"/> or <paramref name="secondIndex"/> is less than 0 or above <see cref="int.MaxValue"/>
+        /// </exception>
         public void Unify(int firstIndex, int secondIndex)
         {
             if (firstIndex < 0 || firstIndex > this.elements.Length - 1)
