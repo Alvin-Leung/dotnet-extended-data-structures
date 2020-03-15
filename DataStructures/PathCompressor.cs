34548636ebdedc6940a610ac6862bf41ffcf3716
alvin <alvin.leung92@gmail.com>
2020-03-14 22:02:38 -0700

    Add xml comments to PathCompressor class. Change Compress method argument names.

--- DataStructures/PathCompressor.cs
+++ DataStructures/PathCompressor.cs
@@ -1,22 +1,34 @@
 ﻿namespace DataStructures
 {
+    /// <summary>
+    /// A path compression strategy class for the <see cref="UnionFind"/> data structure. 
+    /// </summary>
     internal class PathCompressor
     {
         private readonly int[] elements;
 
+        /// <summary>
+        /// Creates an instance of a <see cref="PathCompressor"/>
+        /// </summary>
+        /// <param name="elements">An array of all elements that could undergo path compression</param>
         public PathCompressor(int[] elements)
         {
             this.elements = elements;
         }
 
-        public void Compress(int from, int to)
+        /// <summary>
+        /// Compresses the path from a child element to its root parent element.
+        /// </summary>
+        /// <param name="fromIndex">The index of the child element from which path compression will start. Path compression includes the child element.</param>
+        /// <param name="toIndex">The index of the root parent element where path compression will end.</param>
+        public void Compress(int fromIndex, int toIndex)
         {
-            var next = from;
+            var next = fromIndex;
 
-            while (this.elements[next] != to)
+            while (this.elements[next] != toIndex)
             {
                 var temp = this.elements[next];
-                this.elements[next] = to;
+                this.elements[next] = toIndex;
                 next = temp;
             }
         }
