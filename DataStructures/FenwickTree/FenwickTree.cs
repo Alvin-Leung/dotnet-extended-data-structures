9af7f4879a4d8753232f82d8e4041cd2d1c8ed15
alvin <alvin.leung92@gmail.com>
2020-03-29 17:36:20 -0700

    Create FenwickTree class and corresponding unit test class. Implement constructor, GetValue, and GetSum public methods.

---
+++ DataStructures/FenwickTree/FenwickTree.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class FenwickTree
+    {
+		private int[] sums;
+
+		public FenwickTree(int[] elements)
+		{
+			if (elements == null)
+			{
+				throw new ArgumentNullException(nameof(elements));
+			}
+
+			if (elements.Length == 0)
+			{
+				throw new ArgumentException("Length of input array cannot be null", nameof(elements));
+			}
+
+			this.sums = new int[elements.Length + 1];
+			Array.Copy(sourceArray: elements, sourceIndex: 0, destinationArray: this.sums, destinationIndex: 1, elements.Length);
+
+			for (var i = 1; i <= elements.Length; i++)
+			{
+				var currentSum = this.sums[i];
+				var parentIndex = i + GetLeastSignificantBit(i);
+
+				if (parentIndex >= this.sums.Length)
+				{
+					continue;
+				}
+
+				this.sums[parentIndex] += currentSum;
+			}
+		}
+
+		public int GetValue(int index)
+		{
+			var shiftedIndex = index + 1;
+			return this.GetPrefixSum(shiftedIndex) - this.GetPrefixSum(shiftedIndex - 1);
+		}
+
+		public int GetSum(int from, int to)
+		{
+			if (to < from)
+			{
+				throw new ArgumentException("To value must be greater than or equal to from value");
+			}
+
+			if (from < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(from));
+			}
+
+			if (to < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(to));
+			}
+
+			var shiftedFrom = from + 1;
+			var shiftedTo = to + 1;
+
+			return this.GetPrefixSum(shiftedTo) - this.GetPrefixSum(shiftedFrom - 1);
+		}
+
+		private int GetPrefixSum(int index)
+		{
+			var currentIndex = index;
+			var prefixSum = 0;
+
+			while (currentIndex != 0)
+			{
+				prefixSum += this.sums[currentIndex];
+				currentIndex -= this.GetLeastSignificantBit(currentIndex);
+			}
+
+			return prefixSum;
+		}
+
+		private int GetLeastSignificantBit(int index)
+		{
+			return index & -index;
+		}
+	}
+}
---
+++ UnitTests/FenwickTree/FenwickTreeTests.cs
@@ -0,0 +1,29 @@
+﻿using DataStructures;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    internal class FenwickTreeTests
+    {
+        [TestCase(3, 7, 19)]
+        [TestCase(1, 5, 24)]
+        [TestCase(0, 7, 24)]
+        [TestCase(0, 0, 3)]
+        [TestCase(7, 7, -8)]
+        [Test]
+        public void TestGetSum(int from, int to, int expectedSum)
+        {
+            var fenwickTree = new FenwickTree(new[] { 3, 4, -2, 7, 4, 11, 5, -8 });
+            Assert.That(fenwickTree.GetSum(from, to), Is.EqualTo(expectedSum));
+        }
+
+        [TestCase(0, 4)]
+        [TestCase(29, -36)]
+        [TestCase(10, 49)]
+        public void TestGetValue(int index, int expectedValue)
+        {
+            var fenwickTree = new FenwickTree(new[] { 4, -47, 42, -39, -12, -4, 37, -34, 30, -5, 49, 0, 25, 13, 19, -40, 21, -25, -22, -29, -35, 23, 10, 20, 11, 7, 45, -7, 38, -36 });
+            Assert.That(fenwickTree.GetValue(index), Is.EqualTo(expectedValue));
+        }
+    }
+}
