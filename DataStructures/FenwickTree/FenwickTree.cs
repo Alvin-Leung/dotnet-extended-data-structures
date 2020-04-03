f41c4c9817045421f89b216827b4360053d761b1
alvin <alvin.leung92@gmail.com>
2020-04-02 22:49:00 -0700

    Add xml comments to FenwickTree.cs

--- DataStructures/FenwickTree/FenwickTree.cs
+++ DataStructures/FenwickTree/FenwickTree.cs
@@ -2,11 +2,20 @@
 
 namespace DataStructures
 {
-    public class FenwickTree
+	/// <summary>
+	/// Implementation of a Fenwick/Binary-Indexed Tree for performing range sums and point insertions in O(log(n)) time
+	/// </summary>
+	public class FenwickTree
     {
 		private int[] originalElements;
 		private int[] sums;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FenwickTree"/> class.
+		/// </summary>
+		/// <param name="elements">The element array which range sums will be calculated on. Expected to be zero indexed.</param>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="elements"/> is null</exception>
+		/// <exception cref="ArgumentException">Thrown when <paramref name="elements"/> is an empty array</exception>
 		public FenwickTree(int[] elements)
 		{
 			if (elements == null)
@@ -39,11 +48,24 @@ namespace DataStructures
 			}
 		}
 
+		/// <summary>
+		/// Gets the value of the element at the inputted <paramref name="index"/>
+		/// </summary>
+		/// <param name="index">The index to get a value at</param>
+		/// <returns>The value of the original element at <paramref name="index"/></returns>
 		public int GetValue(int index)
 		{
 			return this.originalElements[index];
 		}
 
+		/// <summary>
+		/// Get the range sum from one index to another (both inclusive)
+		/// </summary>
+		/// <param name="from">The index from which to do the range sum. The range sum will include the value at this index.</param>
+		/// <param name="to">The index to which to do the range sum. The range sum will include the value at this index.</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException">Thrown when <paramref name="to"/> is less than <paramref name="from"/></exception>
+		/// <exception cref="ArgumentOutOfRangeException">Thrown when either the <paramref name="from"/> or <paramref name="to"/> are less than 0</exception>
 		public int GetSum(int from, int to)
 		{
 			if (to < from)
@@ -67,8 +89,19 @@ namespace DataStructures
 			return this.GetPrefixSum(shiftedTo) - this.GetPrefixSum(shiftedFrom - 1);
 		}
 
+		/// <summary>
+		/// Sets the value of the element at the inputted <paramref name="index"/>
+		/// </summary>
+		/// <param name="index">The index to set a value at</param>
+		/// <param name="newValue">The new value to set at <paramref name="index"/></param>
+		/// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="index"/> is less than zero</exception>
 		public void SetValue(int index, int newValue)
 		{
+			if (index < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(index));
+			}
+
 			var delta = newValue - this.originalElements[index];
 			var currentIndex = index + 1;
 
