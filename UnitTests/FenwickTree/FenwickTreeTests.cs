4f8725f7f134eabe61df8184dcf085f2705094b4
alvin <alvin.leung92@gmail.com>
2020-04-02 22:52:51 -0700

    Add xml comments to FenwickTreeTests.cs

--- UnitTests/FenwickTree/FenwickTreeTests.cs
+++ UnitTests/FenwickTree/FenwickTreeTests.cs
@@ -3,8 +3,17 @@ using NUnit.Framework;
 
 namespace UnitTests
 {
+    /// <summary>
+    /// Unit tests for the <see cref="FenwickTree"/> class
+    /// </summary>
     internal class FenwickTreeTests
     {
+        /// <summary>
+        /// Checks that <see cref="FenwickTree.GetSum(int, int)"/> behaves as expected
+        /// </summary>
+        /// <param name="from">The index to start the range sum</param>
+        /// <param name="to">The index to do the range sum up to</param>
+        /// <param name="expectedSum">The expected range sum</param>
         [TestCase(3, 7, 19)]
         [TestCase(1, 5, 24)]
         [TestCase(0, 7, 24)]
@@ -17,6 +26,11 @@ namespace UnitTests
             Assert.That(fenwickTree.GetSum(from, to), Is.EqualTo(expectedSum));
         }
 
+        /// <summary>
+        /// Checks that <see cref="FenwickTree.GetValue(int)"/> behaves as expected
+        /// </summary>
+        /// <param name="index">The index to get a value from</param>
+        /// <param name="expectedValue">The expected value at <paramref name="index"/></param>
         [TestCase(0, 4)]
         [TestCase(29, -36)]
         [TestCase(10, 49)]
@@ -26,6 +40,14 @@ namespace UnitTests
             Assert.That(fenwickTree.GetValue(index), Is.EqualTo(expectedValue));
         }
 
+        /// <summary>
+        /// Checks that <see cref="FenwickTree.SetValue(int, int)"/> behaves as expected
+        /// </summary>
+        /// <param name="index">The index to set a value at</param>
+        /// <param name="valueToSet">The value to set at <paramref name="index"/></param>
+        /// <param name="from">The index to start the range sum</param>
+        /// <param name="to">The index to do the range sum up to</param>
+        /// <param name="expectedSum">The expected range sum</param>
         [TestCase(0, 10, 0, 4, 23)]
         [TestCase(7, 8, 5, 7, 24)]
         [TestCase(4, 12, 3, 5, 30)]
